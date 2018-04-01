300285465d8c0282619eadb4e51229e95d3588b0
“stevengo497” <ngo.steve497@yahoo.com>
2018-03-31 18:22:13 -0600

    li margin corrected

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// Create input box - DONE
+// Create input box 
 // "submit" box with text (Add) - create function
 // numbered list appears under (stacked on top of each other)
-//*******
-// $('#TodoForm').on("submit", function(event) {
-// 	event.preventDefault();
 
 $(document).ready(function(event) {
 
 $('#TodoForm').submit(function(event) {
 	event.preventDefault();
-	// $('#inputBox').reset();
-
 
 $('#orderedList').append("<li>" + ($('#inputBox').val()))
+
+$('#inputBox').val(" "); // this clears the input box
 })
-});
+// $('button').click(function() {
+// 	$('li').strikeout();
+
+})
+
+// Possibly do a strike out w/ css.$('li')
+
+// $('#orderedList').append("<li>" + ($('#inputBox').val()) + ("<button>")) adds a button
